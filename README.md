8838eb42d9e2c49513a4525780bb451d29912686
Nick Saunders <nick@saunde.rs>
2021-09-09 08:08:30 -0700

    Testing

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Flare
-Applicative-style UIs in TypeScript
+# ☕ flare <a href="https://github.com/nsaunders/flare/actions/workflows/verify.yml"><img src="https://shields.io/github/workflow/status/nsaunders/flare/verify" alt="Build status"></a> <a href="https://www.npmjs.com/package/flare"><img src="https://img.shields.io/npm/v/flare.svg" alt="Latest Release"></a> <a href="https://github.com/nsaunders/flare/blob/master/LICENSE"><img src="https://img.shields.io/github/license/nsaunders/flare.svg" alt="License"></a>
+
+**Applicative-style UIs in TypeScript**
---
+++ core/src/lib.test.tsx
@@ -0,0 +1,99 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom/extend-expect";
+import { RunFlare } from "./lib";
+import * as F from "./lib";
+
+describe("checkbox", () => {
+  const initial = true;
+
+  let handler: jest.Mock, checkbox: HTMLInputElement | null | undefined;
+
+  beforeEach(() => {
+    handler = jest.fn();
+    render(
+      <RunFlare
+        flare={F.checkbox({ defaultValue: initial, label: "Label" })}
+        handler={handler}
+      />,
+    );
+    checkbox = screen
+      .getByLabelText("Label")
+      ?.closest("label")
+      ?.querySelector("input");
+  });
+
+  it("renders with default value as initial", () => {
+    expect(checkbox?.checked).toEqual(initial);
+  });
+
+  it("triggers handler on change", () => {
+    checkbox && userEvent.click(checkbox);
+    expect(handler).toHaveBeenLastCalledWith(!initial);
+  });
+});
+
+describe("numberInput", () => {
+  const initial = 5;
+
+  let handler: jest.Mock, input: HTMLInputElement | null | undefined;
+
+  beforeEach(() => {
+    handler = jest.fn();
+    render(
+      <RunFlare
+        flare={F.numberInput({ defaultValue: initial, label: "Label" })}
+        handler={handler}
+      />,
+    );
+    input = screen
+      .getByLabelText("Label")
+      ?.closest("label")
+      ?.querySelector("input");
+  });
+
+  it("renders with default value as initial", () => {
+    expect(input?.value).toEqual(initial.toString());
+  });
+
+  it("triggers handler on change", () => {
+    const updated = 10;
+    if (input) {
+      userEvent.clear(input);
+      userEvent.type(input, updated.toString());
+    }
+    expect(handler).toHaveBeenLastCalledWith(updated);
+  });
+});
+
+describe("textInput", () => {
+  const initial = "Foo";
+
+  let handler: jest.Mock, input: HTMLInputElement | null | undefined;
+
+  beforeEach(() => {
+    handler = jest.fn();
+    render(
+      <RunFlare
+        flare={F.textInput({ defaultValue: initial, label: "Label" })}
+        handler={handler}
+      />,
+    );
+    input = screen
+      .getByLabelText("Label")
+      ?.closest("label")
+      ?.querySelector("input");
+  });
+
+  it("renders with default value as initial", () => {
+    expect(input?.value).toEqual(initial);
+  });
+
+  it("triggers handler on change", () => {
+    const additional = "Bar";
+    if (input) {
+      userEvent.type(input, additional);
+    }
+    expect(handler).toHaveBeenLastCalledWith(initial + additional);
+  });
+});
