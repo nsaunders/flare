e4da0a2165beb52749bd9f7b09659e418912a759
Nick Saunders <nick@saunde.rs>
2021-10-17 05:19:55 -0700

    Remove redundant stuff from mdx type defs.

--- site/mdx.d.ts
+++ site/mdx.d.ts
@@ -9,10 +9,10 @@ declare module "*.mdx" {
 }
 
 declare module "@mdx-js/react" {
-  import { ComponentType, ReactNode, StyleHTMLAttributes } from "react";
+  import { Component, ReactNode } from "react";
 
   type MDXProps = {
-    children: React.ReactNode;
+    children: ReactNode;
     components: Record<"h1" | "h2" | "h3" | "h4" | "h5" | "h6", (props: { children : ReactNode}) => ReactNode>;
   }
   export class MDXProvider extends React.Component<MDXProps> {}
