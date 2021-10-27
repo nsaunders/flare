5b1d2754058fb77068b63fbb9a21759ce93c526c
Nick Saunders <nick@saunde.rs>
2021-10-27 12:31:51 -0700

    Customizing doc

---
+++ site/pages/customizing.tsx
@@ -0,0 +1,17 @@
+import Markdown from "../docs/customizing.mdx";
+import { SmartHead } from "../components/smart-head";
+import { Doc } from "../components/doc";
+
+const Customizing = () => (
+  <>
+    <SmartHead
+      title="Customizing"
+      description="Give your Flare UI a custom look and feel."
+    />
+    <Doc>
+      <Markdown />
+    </Doc>
+  </>
+);
+
+export default Customizing;
